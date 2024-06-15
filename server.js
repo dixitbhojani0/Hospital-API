936e592c429e418883ceede21f17ead05dc4068b
Shivam Patel <shivam.patel@tatvasoft.com>
2024-06-16 02:52:52 +0530

    Remove host ip

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { connectDb } from "./src/config/db.js";
 try {
     server.listen(process.env.PORT, async () => {
         await connectDb();
-        console.log(`Server is running at http://${process.env.HOST}:${process.env.PORT}`);
+        console.log(`server is running at port ${process.env.PORT}`);
     })
 } catch (err) {
     console.log(`server failed with error ${err}`);
